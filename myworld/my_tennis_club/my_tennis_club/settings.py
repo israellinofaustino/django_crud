f1a7c20da707921016c66b2696d6003f4164b28c
israellinofaustino <israellinofaustino@hotmail.com>
2023-07-03 23:00:32 +0100

    new features

--- myworld/my_tennis_club/my_tennis_club/settings.py
+++ myworld/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pc9_6^)vu+s47j^27-)!tg*6lpp4+376fpb)5ur&!&^=i!_qa9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
